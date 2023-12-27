6cce8aa2839637d97762ef2fb15943a75d831363
Yuliia Hots <goculiya@gmail.com>
2023-12-27 14:45:03 +0100

    utilisation du defer pour le chargement JS

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,70 +1,65 @@
-// On attend le chargement de la page avec DOMContentLoaded 
-document.addEventListener("DOMContentLoaded", () => {
+// Variables gloables
+const gallery = document.querySelector(".gallery");
 
-    // Variables gloables
-    const gallery = document.querySelector(".gallery");
+function testWorks(){
+    console.log(fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(works => works.forEach(work => console.log(work))));
+}
 
-    function testWorks(){
-        console.log(fetch("http://localhost:5678/api/works")
-        .then(response => response.json())
-        .then(works => works.forEach(work => console.log(work))));
+//testWorks();
+
+function afficheGallery(type){
+    // Si element HTML active existe on le supprime
+    if(document.querySelector(".btn-active")){
+        document.querySelector(".btn-active").classList.remove("btn-active");
     }
 
-    //testWorks();
-
-    function afficheGallery(type){
-        // Si element HTML active existe on le supprime
-        if(document.querySelector(".btn-active")){
-            document.querySelector(".btn-active").classList.remove("btn-active");
-        }
-
-        // Boucle pour supprimer un par un les elements figures
-        while (gallery.firstChild) {
-            gallery.firstChild.remove();   
-        }
-
-        // Appel de l api pour afficher les "works"
-        fetch("http://localhost:5678/api/works")
-        .then(response => response.json())
-        .then(works => works.forEach(work => {
-                // Filtre sur la categorie ou tt la galerie
-                if (work.categoryId == type || type == 0){
-                        const figure = document.createElement("figure");
-                        figure.innerHTML = "<img src=\"" + work.imageUrl + "\" alt=\"" + work.title + "\">";
-                        const figcaption = document.createElement("figcaption");
-                        figcaption.innerHTML = work.title; 
-                        figure.appendChild(figcaption);
-                        gallery.appendChild(figure);   
-                    }                                             
-                })            
-        )
-
+    // Boucle pour supprimer un par un les elements figures
+    while (gallery.firstChild) {
+        gallery.firstChild.remove();   
     }
 
+    // Appel de l api pour afficher les "works"
+    fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(works => works.forEach(work => {
+            // Filtre sur la categorie ou tt la galerie
+            if (work.categoryId == type || type == 0){
+                    const figure = document.createElement("figure");
+                    figure.innerHTML = "<img src=\"" + work.imageUrl + "\" alt=\"" + work.title + "\">";
+                    const figcaption = document.createElement("figcaption");
+                    figcaption.innerHTML = work.title; 
+                    figure.appendChild(figcaption);
+                    gallery.appendChild(figure);   
+                }                                             
+            })            
+    )
+
+}
+
+afficheGallery(0);
+
+const btnObjet = document.getElementById("btn-objet");
+btnObjet.addEventListener ("click",function() {        
+    afficheGallery(1);
+    btnObjet.classList.add("btn-active");
+});
+
+const btnAppart = document.getElementById("btn-appart");
+btnAppart.addEventListener ("click",function() {        
+    afficheGallery(2);
+    btnAppart.classList.add("btn-active");
+});
+
+const btnHotel = document.getElementById("btn-hotel");
+btnHotel.addEventListener ("click",function() {        
+    afficheGallery(3);
+    btnHotel.classList.add("btn-active");
+});
+
+const btnTous = document.getElementById("btn-tous");
+btnTous.addEventListener ("click",function() {        
     afficheGallery(0);
-
-    const btnObjet = document.getElementById("btn-objet");
-    btnObjet.addEventListener ("click",function() {        
-        afficheGallery(1);
-        btnObjet.classList.add("btn-active");
-    });
-
-    const btnAppart = document.getElementById("btn-appart");
-    btnAppart.addEventListener ("click",function() {        
-        afficheGallery(2);
-        btnAppart.classList.add("btn-active");
-    });
-
-    const btnHotel = document.getElementById("btn-hotel");
-    btnHotel.addEventListener ("click",function() {        
-        afficheGallery(3);
-        btnHotel.classList.add("btn-active");
-    });
-
-    const btnTous = document.getElementById("btn-tous");
-    btnTous.addEventListener ("click",function() {        
-        afficheGallery(0);
-        btnTous.classList.add("btn-active");   
-    });
-
+    btnTous.classList.add("btn-active");   
 });
